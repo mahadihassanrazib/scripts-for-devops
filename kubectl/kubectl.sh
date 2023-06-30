85d985bda5ba99237df69d6cd301194a7ea062a6
Md. Mahadi Hassan Razib <agun21st@outlook.com>
2023-06-30 21:41:56 +0600

    kubectl script added

--- kubectl/kubectl.sh
+++ kubectl/kubectl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
