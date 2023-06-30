09b30f468d47289eaa3604630724eabf383432d8
Md. Mahadi Hassan Razib <agun21st@outlook.com>
2023-06-30 21:24:32 +0600

    terragrunt installation added

--- Terraform/terragrunt.sh
+++ Terraform/terragrunt.sh
@@ -2,8 +2,10 @@
 
 wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.48.0/terragrunt_linux_amd64
 
-sudo mv terragrunt_linux_amd64 terragrunt
+mv terragrunt_linux_amd64 terragrunt
 
-sudo mv terragrunt /usr/local/bin/terragrunt
+chmod u+x terragrunt
 
-sudo terragrunt --version
+mv terragrunt /usr/local/bin/terragrunt
+
+terragrunt -v
